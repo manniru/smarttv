f37357239c39eddba9d3408141ea87af8af3febb
Mario Lamacchia <mario@appstrakt.com>
2015-12-21 01:48:47 +0100

    Open electron on default location

--- electron/index.js
+++ electron/index.js
@@ -25,7 +25,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL(require('../server/storage').data.settings.defaultLocation);
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
---
+++ server/location/index.js
@@ -0,0 +1,15 @@
+var router = require('express').Router(),
+    getWindow = require('../../electron');
+
+router.get('/', function(req, res) {
+    res.json({
+        location: getWindow().getURL()
+    });
+});
+
+router.put('/', function(req, res) {
+    getWindow().loadURL(req.body.location);
+    res.sendStatus(200);
+});
+
+module.exports = router;
