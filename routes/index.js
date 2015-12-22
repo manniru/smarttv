efc6655ef5702d021fa99fa82d3316fc099888c0
Mario Lamacchia <mario@appstrakt.com>
2015-12-23 00:10:17 +0100

    Remove location api

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var router = require('express').Router();
 
 router.use('/settings', require('./settings'));
-router.use('/location', require('./location'));
 router.use('/auth', require('./auth'));
 
 module.exports = router;
