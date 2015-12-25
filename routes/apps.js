1fc879a45b7dfbeb830ddc390ff59e20ed4f6356
Mario Lamacchia <mario@appstrakt.com>
2015-12-25 23:44:37 +0100

    Create apps api

---
+++ routes/apps.js
@@ -0,0 +1,6 @@
+var apps = require('../apps');
+var router = require('express').Router();
+
+router.get('/', function(req, res) {
+    res.json(Object.keys(apps.list));
+});
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ var router = require('express').Router();
 
 router.use('/channel', require('./channel'));
 router.use('/storage', require('./storage'));
+router.use('/apps', require('./apps'));
 
 module.exports = router;
