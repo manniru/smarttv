d7a4d550df2cf7d09ac5275d14d70dbb8662dfec
Mario Lamacchia <mario@appstrakt.com>
2016-01-07 22:37:26 +0100

    Serve apps

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ var path = require('path'),
 require('./socket')(server);
 
 app.use(require('body-parser').json());
-app.use(express.static(path.join(
-    __dirname + '/node_modules/' + apps.getCurrent() + '/client'
-)));
+
 app.use('/api', require('./routes'));
+
+// Serve apps
+app.get('/', function(req, res) {
+    res.redirect('/smarttv-' + apps.getCurrent() + '/client/');
+});
+app.use(function(req, res, next) {
+    // Check requeste app is the current one
+    if (!req.path.startsWith('/smarttv-' + apps.getCurrent() + '/client/')) {
+        res.redirect('/');
+    } else {
+        next();
+    }
+});
+app.use(express.static(__dirname + '/node_modules'));
 
 var port = process.env.npm_config_port || 8000;
 console.log('app is listening on port ' + port);
