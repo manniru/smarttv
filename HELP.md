4b5a5ee10f9c27f7f9ca07bc07fce6674723521c
Mario Lamacchia <mario@appstrakt.com>
2016-01-07 21:45:37 +0100

    Add npm tasks

---
+++ HELP.md
@@ -0,0 +1,10 @@
+SmartTV
+
+Usage (with npm): npm <command> [-- <args>]
+
+Running:
+    npm start [--no-fullscreen]: run the SmartTV [windowed mode]
+    npm run api: run the SmartTV APIs
+
+Help
+    npm run help: print this help
--- electron.js
+++ electron.js
@@ -1,50 +1,52 @@
-if (!process.env.npm_config_only_server) {
-    const electron = require('electron');
-    const app = electron.app;  // Module to control application life.
-    const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
-
-    // Report crashes to our server.
-    electron.crashReporter.start();
-
-    // Keep a global reference of the window object, if you don't, the window will
-    // be closed automatically when the JavaScript object is garbage collected.
-    exports.mainWindow = null;
-
-    // Quit when all windows are closed.
-    app.on('window-all-closed', function() {
-      // On OS X it is common for applications and their menu bar
-      // to stay active until the user quits explicitly with Cmd + Q
-      if (process.platform != 'darwin') {
-        app.quit();
-      }
-    });
-
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    app.on('ready', function() {
-      // Create the browser window.
-      exports.mainWindow = new BrowserWindow({
-          fullscreen: !process.env.npm_config_windowed
-      });
-
-      if (process.env.npm_config_dev_tools) {
-          exports.mainWindow.openDevTools();
-      }
-
-      if (typeof exports.onReady === 'function') {
-          exports.onReady();
-      }
-
-      // Emitted when the window is closed.
-      exports.mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        exports.mainWindow = null;
-      });
-    });
-} else {
+try {
+    var electron = require('electron');
+} catch(e) {
     exports.mainWindow = {
         loadURL: function() {}
     };
+    return;
 }
+
+var app = electron.app;  // Module to control application life.
+var BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+
+// Report crashes to our server.
+electron.crashReporter.start();
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+exports.mainWindow = null;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  exports.mainWindow = new BrowserWindow({
+      fullscreen: !process.env.npm_config_windowed
+  });
+
+  if (process.env.npm_config_dev_tools) {
+      exports.mainWindow.openDevTools();
+  }
+
+  if (typeof exports.onReady === 'function') {
+      exports.onReady();
+  }
+
+  // Emitted when the window is closed.
+  exports.mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    exports.mainWindow = null;
+  });
+});
