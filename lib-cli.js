18296e258719fe6b9056cef4ae8b734ad0cd1ac7
Mario Lamacchia <mario@appstrakt.com>
2016-01-12 23:40:50 +0100

    lib-cli: add ready and getPath functions

--- lib-cli.js
+++ lib-cli.js
@@ -8,14 +8,24 @@
   var socket = io.connect('/');
   smarttv.send = function(msg) {
     socket.emit('message', msg);
+    return smarttv;
   };
   smarttv.on = function(c, fn) {
+    if (c === 'ready') {
+      return smarttv.ready(fn);
+    }
     socket.on(c, fn);
+    return smarttv;
+  };
+  smarttv.ready = function(fn) {
+    $('body').on('smarttv-ready', fn);
+    return smarttv;
   };
 
   // Apps
   $.get('/api/apps', function(apps) {
     smarttv.apps = apps;
+    $('body').trigger('smarttv-ready');
   });
   smarttv.showApp = function(app) {
     window.location.href = '/' + app;
@@ -23,17 +33,7 @@
   smarttv.pressKey = function(key) {
     sendEvent('keyDown', key);
     sendEvent('keyUp', key);
-
-    function sendEvent(type, key) {
-      $.ajax('/api/inputs', {
-        method: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-          type: type,
-          keyCode: key
-        })
-      });
-    }
+    return smarttv;
   };
 
   // Hidden keyboard
@@ -47,6 +47,27 @@
   });
   smarttv.showKeyboard = function() {
     $('#st-hidden-input').focus();
+    return smarttv;
   };
+  smarttv.getPath = function(app) {
+    return '/' + app.name + '/client';
+  };
+  smarttv.getPublicPath = function(app) {
+    if (!app) {
+      return '../public';
+    }
+    return '/' + app.name + '/public';
+  };
+
+  function sendEvent(type, key) {
+    $.ajax('/api/inputs', {
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        type: type,
+        keyCode: key
+      })
+    });
+  }
 
 })(window, $, io);
